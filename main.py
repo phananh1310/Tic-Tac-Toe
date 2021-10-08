db019d27cf42650a65612039781eb70f7fb43cb1
phananh1310 <phananh13102001@gmail.com>
2021-10-08 19:34:38 +0700

    Update main.py
    Print the white board only

--- main.py
+++ main.py
@@ -1,13 +1,40 @@
 import pygame, sys
 from pygame.locals import *
 
-pygame.init()
-DISPLAYSURF = pygame.display.set_mode((600, 600))
-pygame.display.set_caption('Tic Tac Toe Game')
+WINDOWWIDTH  = 600
+WINDOWHEIGHT = 600
+FPS = 30
 
-while True: # main game loop
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-    pygame.display.update()
+
+         # R G B
+GRAY     = (100, 100, 100)
+NAVYBLUE = ( 60, 60, 100)
+WHITE    = (255, 255, 255)
+RED      = (255, 0, 0)
+GREEN    = ( 0, 255, 0)
+BLUE     = ( 0, 0, 255)
+YELLOW   = (255, 255, 0)
+ORANGE   = (255, 128, 0)
+PURPLE   = (255, 0, 255)
+CYAN     = ( 0, 255, 255)
+
+
+def main():
+    pygame.init()
+    global FPSCLOCK, DISPLAYSURF
+    FPSCLOCK = pygame.time.Clock()
+    DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+    pygame.display.set_caption('Tic Tac Toe Game')
+    mousex = 0 # used to store x coordinate of mouse event
+    mousey = 0 # used to store y coordinate of mouse event
+
+    while True: # main game loop
+        DISPLAYSURF.fill(WHITE)
+        for event in pygame.event.get():
+            if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
+                pygame.quit()
+                sys.exit()
+
+        pygame.display.update()
+        FPSCLOCK.tick(FPS)
+main()
